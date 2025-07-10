7e1395aa660e5764dc935547606307539546ad6b
Lucien Thomas <lucienthomas00@gmail.com>
2025-07-10 02:24:01 -0500

    remove click

--- src/prreview/__init__.py
+++ src/prreview/__init__.py
@@ -1,16 +1,15 @@
-import click
-
 from prreview.review.reviewer import Reviewer
 
+REPO_PATH = "./.input_codebase"
+BASE_BRANCH = "main"
+HEAD_BRANCH = "dev"
 
-@click.command()
-@click.option("--repo-path", default="./.input_codebase", help="Target repository")
-@click.option("--base-branch", default="main", help="Base branch")
-@click.option("--head-branch", default="dev", help="Head branch")
-def main(repo_path: str, base_branch: str, head_branch: str) -> None:
+
+
+def main() -> None:
     """Run the PR reviewer on *repo_path* between two branches."""
     reviewer = Reviewer()
-    reviewer.run(repo_path=repo_path, base_branch=base_branch, head_branch=head_branch)
+    reviewer.run(repo_path=REPO_PATH, base_branch=BASE_BRANCH, head_branch=HEAD_BRANCH)
 
 
 
